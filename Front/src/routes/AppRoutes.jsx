d1596bd642f99474d3dba4b5c34df2f2a91a9522
leonAsturizaga <leonasturizaga@gmail.com>
2024-10-11 11:09:27 -0300

    basic home + login + dashboard

---
+++ Front/src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from '../components/Auth/Login';
+import Register from '../components/Auth/Register';
+import LandingPage from '../components/Home/LandingPage';
+import AdminDashboard from '../components/Dashboard/AdminDashboard';
+
+const AppRoutes = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRoutes;
