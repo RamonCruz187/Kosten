56b4f0e16c3042cda4a055dd484388a4b2196468
matias-saig <saig.matias@gmail.com>
2024-10-15 18:00:10 -0300

    front: theme breakpoint

--- Front/theme.js
+++ Front/theme.js
@@ -10,10 +10,10 @@ const theme = createTheme({
   breakpoints: {
     values: {
       xs: 0,
-      sm: 750,
+      sm: 600,
       md: 900,
       lg: 1200,
-      xl: 1536,
+      xl: 1440,
     },
   },
   palette: {
