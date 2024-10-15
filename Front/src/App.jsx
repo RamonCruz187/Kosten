554e79fb9e7188f9e1823586708a49cff3f76f72
hiram_chavez <hiram.chavez@dataiq.com.mx>
2024-10-14 21:42:08 -0600

    feat: add toggle theme and toggle color mode

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 import {GlobalContextProvider} from "./shared/context/GlobalContext.jsx";
+import {GlobalThemeContextProvider} from "./shared/theme/GlobalThemeContext.jsx";
 
 const App = () => {
   return (
       <GlobalContextProvider>
-        <AppRoutes />
+          <GlobalThemeContextProvider>
+              <AppRoutes />
+          </GlobalThemeContextProvider>
       </GlobalContextProvider>
   );
 };
--- Front/src/components/Home/LandingPage.jsx
+++ Front/src/components/Home/LandingPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import AppAppBar from "../../shared/components/AppAppBar.jsx";
 
 const LandingPage = () => {
   return (
     <div>
+        <AppAppBar />
       <h1>Welcome to the App</h1>
       <p>This is the landing page.</p>
     </div>
