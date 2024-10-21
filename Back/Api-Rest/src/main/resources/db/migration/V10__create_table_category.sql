d573e8366f82b6a5236e4af0a9c2da6d157b8073
RamonCruz187 <ramon.cruz187@gmail.com>
2024-10-21 13:35:18 -0300

    feat: create Category Entity

---
+++ Back/Api-Rest/src/main/resources/db/migration/V10__create_table_category.sql
@@ -0,0 +1,7 @@
+create table category (
+                       id BIGINT NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(255) NULL DEFAULT NULL,
+                       package_id BIGINT NULL DEFAULT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (package_id)
+                           REFERENCES packages (id));
