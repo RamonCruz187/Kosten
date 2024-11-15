9fd121281f0725ba004354c10b909d90a6f4df39
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-11-15 17:53:40 -0300

    md Login - se pasan props para cuando es modal

--- Front/src/components/Auth/Login.jsx
+++ Front/src/components/Auth/Login.jsx
@@ -3,14 +3,15 @@ import {Button, Typography, Stack, CircularProgress} from "@mui/material";
 import { login } from "../../api/authApi.js";
 import { NotificationService } from "../../shared/services/notistack.service.jsx";
 import { useAuth } from "../../shared/hooks/useAuth.jsx";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import InputNormal from "./InputNormal.jsx";
 import InputPassword from "./InputPassword.jsx";
 import {getData} from "../../api/userApi.js";
 import {useUserData} from "../../shared/hooks/useUserData.jsx";
 import Box from "@mui/material/Box";
 
-const Login = () => {
+const Login = ({handleClose=() => {}}) => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isFetching, setIsFetching] = useState(false);
@@ -18,7 +19,10 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
-
+  const handleGoToRegister = () => {
+    handleClose();
+    navigate("/register");
+  };
   const { handleLogin } = useAuth();
   const{ setUserData } = useUserData();
 
@@ -53,6 +57,7 @@ const Login = () => {
           alignItems: "center",
           width: 400,
           padding: "1rem 2rem",
+          background: "white",
         }}
       >
         <Typography variant="titleH2">LOGIN</Typography>
@@ -66,17 +71,15 @@ const Login = () => {
           fxIcon={handleClickShowPassword} 
         
         />
-
+{/* 
         <Typography variant="buttonMini">
           olvidé mi contraseña
-        </Typography>
+        </Typography> */}
 
         {
           isFetching ? (
               <Box sx={{ display: 'flex', flexDirection: 'column', width: "50%", textAlign: 'center', alignItems: 'center', p: 1, gap: 1 }}>
-                <CircularProgress
-                    size={20}
-                />
+                <CircularProgress size={20}/>
                 <Typography variant="caption">Cargando...</Typography>
               </Box>
           ) : (
@@ -86,11 +89,11 @@ const Login = () => {
           )
         }
 
-        <Link to="/register" style={{width:'50%'}}>
-        <Button color="transparentButton" disableElevation sx={{width:"100%"}}>
+        <Button color="transparentButton" disableElevation sx={{width:'50%'}}
+          onClick={handleGoToRegister}
+        >
           Registrarse
         </Button>
-        </Link>
       </Stack>
     </form>
   );
