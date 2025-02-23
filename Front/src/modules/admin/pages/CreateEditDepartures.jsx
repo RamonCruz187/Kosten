60945e0a02e50654e65f541b4277fe4697e9d1d7
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-23 17:29:16 -0300

    md CreateEditDepartures - se actualizan botones, se agregan espacios entre formularios, se les pasa el refetch, se borran comentarios y console.logs, se actualizan los colores con la paleta

--- Front/src/modules/admin/pages/CreateEditDepartures.jsx
+++ Front/src/modules/admin/pages/CreateEditDepartures.jsx
@@ -1,5 +1,5 @@
 // Front/src/modules/admin/components/CreateEditDepartures.jsx
-import { Box, Button, CircularProgress, Typography } from "@mui/material"
+import { Box, CircularProgress, Typography, useTheme } from "@mui/material"
 import { useParams } from "react-router-dom";
 import { DepartureForm } from "../components/DepartureForm";
 import { RiAddLargeLine } from "react-icons/ri";
@@ -7,15 +7,17 @@ import { ModalInscripts } from "../components/ModalInscripts";
 import { useCallback, useEffect, useState } from "react";
 import { getPackageById } from "@/api/packageApi";
 import { NotificationService } from "@/shared/services/notistack.service";
+import { WhiteButton } from "@/shared/components/buttons/WhiteButton";
 
 
 export const CreateEditDepartures = () => {
   const params = useParams();
   const packageFromCategory = +params.id || null;
+  const theme = useTheme();
+  const { palette } = theme
 
   const [isFetching, setIsFetching] = useState(true);
   const [packageData, setPackageData] = useState(true);
-  // const [allStaff, setAllStaff] = useState(null);
   const [openModal, setOpenModal] = useState(null);
   const [indexDepartures, setIndexDepartures] = useState(null);
   const [showCreateForm, setShowCreateForm] = useState(false);
@@ -29,13 +31,12 @@ export const CreateEditDepartures = () => {
     setIsFetching(true);
     try {
         const response = await getPackageById(packageFromCategory); // Axios devuelve 'data' directamente
-        console.log('data', response?.data?.data);
         setPackageData(response?.data?.data);
-        NotificationService.success('El paquete fue cargado con éxito');
-        console.log('El paquete fue cargado con éxito');
+        NotificationService.success('Las salidas fueron cargadas con éxito');
+        console.log('Las salidas fueron cargadas con éxito');
     } catch (error) {
         console.error(error);
-        NotificationService.error('Error al cargar el paquete');
+        NotificationService.error('Error al cargar las salidas');
     } finally {
         setIsFetching(false);
     }
@@ -59,12 +60,17 @@ export const CreateEditDepartures = () => {
     );
   }
   return (
-    <Box sx={{maxWidth: '1100px', margin: '1rem auto', paddingBottom: '2rem', backgroundColor: "#F3F3F3"}} >
+    <Box sx={{
+      maxWidth: '1100px', 
+      margin: '1rem auto', 
+      paddingBottom: '2rem',
+      display: 'flex',
+      flexDirection: 'column',
+    }} >
       {/* imagen con titulo */}
       <Box
         sx={{
           height: '180px',
-          backgroundColor: '#747474',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
@@ -79,7 +85,7 @@ export const CreateEditDepartures = () => {
           <Typography
             variant="titleH3"
             sx={{ 
-              color: '#fff', 
+              color: palette.tertiary.light, 
               fontSize: '36px',
               fontWeight: '600',
               lineHeight: '20.8px',
@@ -90,54 +96,42 @@ export const CreateEditDepartures = () => {
           </Typography>
         </Box>
       </Box>
-
-      {/* formularios */}
-      {/* hacer los maps */}
-        {/* Mapea las salidas existentes */}
-        {packageData?.departures?.length > 0 &&
-          packageData.departures.map((departure, index) => (
-            <DepartureForm
-              key={`departure-${departure.id}`}
-              departureData={departure}
-              package_Id={packageData.id}
-              // allStaff={allStaff}
-              setOpenModal={()=>handleOpenModal(departure, index)}
-              index={index}
-              onActionComplete={fetchPackageById}
-            />
-          ))}
-        {/* Botón para crear nueva */}
-        {packageData?.departures?.length > 0 &&
-          <Button
-            variant="contained"
-            color="#D9D9D9"
-            onClick={() => setShowCreateForm(true)}
-            sx={{ margin: 2, 
-              boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-              borderRadius: '7px',
-              display: 'flex',
-              gap: '.5rem',
-              alignItems: 'center',
-              backgroundColor: '#D9D9D9'
-            }}
-          >
-            <RiAddLargeLine />
-            <Typography sx={{fontSize: '0.9rem'}}>
-            AGREGAR SALIDA
-            </Typography>
-          </Button>
-        }
-        {/* Formulario para crear nueva salida */}
-        {(showCreateForm || packageData?.departures?.length === 0) && (
+    {/* formularios */}
+    {/* hacer los maps */}
+      {/* Mapea las salidas existentes */}
+      {packageData?.departures?.length > 0 &&
+        packageData.departures.map((departure, index) => (
           <DepartureForm
-            departureData={{startDate: "", endDate: "" }}
+            key={`departure-${departure.id}`}
+            departureData={departure}
             package_Id={packageData.id}
-            // setOpenModal={()=>handleOpenModal(departure, index)}
-            isCreate={true}
-            onActionComplete={fetchPackageById} // Refetch tras completar la acción
+            setOpenModal={()=>handleOpenModal(departure, index)}
+            index={index}
+            refetch={fetchPackageById}
           />
-        )}
-        <ModalInscripts openModal={openModal} setOpenModal={handleOpenModal} indexDepartures={indexDepartures} />
+        ))}
+      {/* Botón para crear nueva */}
+      {packageData?.departures?.length > 0 &&
+        <WhiteButton
+          onClick={() => setShowCreateForm(true)}
+          text="AGREGAR SALIDA"
+          icon={<RiAddLargeLine />}
+          sx={{
+            marginBottom: "1rem",
+          }}
+        />
+      }
+      {/* Formulario para crear nueva salida */}
+      {(showCreateForm || packageData?.departures?.length === 0) && (
+        <DepartureForm
+          departureData={{startDate: "", endDate: "" }}
+          package_Id={packageData.id}
+          isCreate={true}
+          refetch={fetchPackageById}
+        />
+      )}
+      <ModalInscripts openModal={openModal} setOpenModal={handleOpenModal} indexDepartures={indexDepartures} />
+        
     </Box>
   )
 }
