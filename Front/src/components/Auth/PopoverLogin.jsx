2671451c52160e8904584ea57eaf8056b60f72d9
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-02-27 02:56:54 -0300

    md PopoverLogin - se acomodan estilos, se hace responsivo

--- Front/src/components/Auth/PopoverLogin.jsx
+++ Front/src/components/Auth/PopoverLogin.jsx
@@ -9,7 +9,14 @@ const PopoverLogin = ({ isOpenLogin=false, handleClose }) => {
        open={isOpenLogin}
        onClose={handleClose}
       >
-        <Box sx={{ position: "absolute", top: "20px", right: "20px"}}>
+        <Box sx={{ 
+          position: "absolute", 
+          top: {xs: "200px", sm: "20px", md: "100px", xl:"120px"}, 
+          right: {xs: "1.5dvw", sm: "50%", md: "100px", xl:"120px"},
+          zIndex: 100,
+          width: {xs: "97dvw", sm: "unset"},
+          transform: {sm: "translate(50%, 50%)", md: "unset"},
+        }}>
           <Login handleClose={handleClose} isModal={true} />
         </Box>
       </Modal>
