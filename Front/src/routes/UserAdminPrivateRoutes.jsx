c01ba12811917c6e3e0c46ca57dd7e3e2eca6713
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-09 21:26:38 -0300

    fix UserAdminPrivateRoutes - se agrega un loader cuando verifica autentificacion y rol y eso permite el tiempo para que no haga el flash redireccionando a login

--- Front/src/routes/UserAdminPrivateRoutes.jsx
+++ Front/src/routes/UserAdminPrivateRoutes.jsx
@@ -5,20 +5,30 @@
 import { Navigate } from 'react-router-dom';
 import {useAuth} from "../shared/hooks/useAuth.jsx";
 import {useUserData} from "../shared/hooks/useUserData.jsx";
+import Loading from '@/shared/components/Loading.jsx';
 
 export function UserAdminPrivateRoutes({ children }) {
 
     const { isAuthenticated } = useAuth();
     const { user } = useUserData();
 
+    // Agregar un estado de loading mientras se cargan los datos
+    if (!isAuthenticated && localStorage.getItem("userAuth")) {
+        // Está cargando los datos del localStorage
+        return <Loading />;
+    }
+
+    // Si no está autenticado, redirigir al login
     if ( !isAuthenticated ) {
         return <Navigate to="/login" replace />;
     }
 
+    // Si está autenticado pero no es admin, redirigir al inicio
     if ( user.role !== 'ADMIN' ) {
         return <Navigate to="/" replace />;
     }
 
+    // Si está autenticado y es admin, mostrar el contenido
     return children;
 
 }
